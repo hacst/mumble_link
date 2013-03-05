a3da1f112df5209a3a95a6cafd924d7751d5b6bc
Stefan Hacker <dd0t@users.sourceforge.net>
2013-03-05 23:02:50 +0100

    Add getters and missing setters.
    - Can now use open() to open the shared memory interface without writing to it. Useful for peeking on other plugins operations.
    - Can now use start() to restart auto-ticking after stopping it with stop()
    - Add basic python doc strings for all mapped functions
    - Update windows binary

--- mumble_link.cpp
+++ mumble_link.cpp
@@ -71,7 +71,7 @@ struct LinkedMem {
 
 LinkedMem *lm = NULL;
 
-bool run_updater = true;
+bool run_updater = false;
 thread t;
 
 void updater() {
@@ -80,9 +80,8 @@ void updater() {
 		lm->uiTick++;
 	}
 }
-	
-bool setup(const wstring& name, const wstring& description) {
 
+bool link_open() {
 #ifdef WIN32
 	HANDLE hMapObject = OpenFileMappingW(FILE_MAP_ALL_ACCESS, FALSE, L"MumbleLink");
 	if (hMapObject == NULL)
@@ -111,18 +110,33 @@ bool setup(const wstring& name, const wstring& description) {
 		return false;
 	}
 #endif
-
-	memset(lm, 0, sizeof(LinkedMem));
-	wcsncpy(lm->name, name.c_str(), 256);
-	wcsncpy(lm->description, description.c_str(), 2048);
-	lm->uiVersion = 2;
-	t = thread(updater);
 	return true;
 }
 
 void stop() {
 	run_updater = false;
 	t.join();
+}
+
+void start() {
+	if (run_updater) return;
+
+	run_updater = true;
+	t = thread(updater);
+}
+
+
+bool setup(const wstring& name, const wstring& description) {
+	if (!link_open())
+		return false;
+
+	memset(lm, 0, sizeof(LinkedMem));
+	wcsncpy(lm->name, name.c_str(), 256);
+	wcsncpy(lm->description, description.c_str(), 2048);
+	lm->uiVersion = 2;
+	
+	start();
+	return true;
 }
 
 void set_name(const wstring& name) {
@@ -133,6 +147,35 @@ void set_description(const wstring& description) {
 	wcsncpy(lm->description, description.c_str(), 2048);
 }
 
+void set_version(const uint32_t version) {
+	lm->uiVersion = version;
+}
+
+void set_tick(const uint32_t tick) {
+	lm->uiTick = tick;
+}
+
+uint32_t get_tick() {
+	return lm->uiTick;
+}
+
+void tick() {
+	++lm->uiTick;
+}
+
+wstring get_name() {
+	return lm->name;
+}
+
+wstring get_description() {
+	return lm->description;
+}
+
+uint32_t get_version() {
+	return lm->uiVersion;
+}
+
+
 void set_context(const string& context) {
 	memcpy(lm->context, context.c_str(), context.size());
 	lm->context_len = context.size();
@@ -142,8 +185,12 @@ void set_identity(const wstring& identity) {
 	wcsncpy(lm->identity, identity.c_str(), 256);
 }
 
-void set_version(const uint32_t version) {
-	lm->uiVersion = version;
+string get_context() {
+	return string((char*)lm->context, lm->context_len);
+}
+
+wstring get_identity() {
+	return lm->identity;
 }
 
 void set_camera_front(const float x, const float y, const float z) {
@@ -162,6 +209,18 @@ void set_camera_position(const float x, const float y, const float z) {
 	lm->fCameraPosition[0] = x;
 	lm->fCameraPosition[1] = y;
 	lm->fCameraPosition[2] = z;
+}
+
+boost::python::tuple get_camera_front() {
+	return boost::python::make_tuple(lm->fCameraFront[0], lm->fCameraFront[1], lm->fCameraFront[2]);
+}
+
+boost::python::tuple get_camera_top() {
+	return boost::python::make_tuple(lm->fCameraTop[0], lm->fCameraTop[1], lm->fCameraTop[2]);
+}
+
+boost::python::tuple get_camera_position() {
+	return boost::python::make_tuple(lm->fCameraPosition[0], lm->fCameraPosition[1], lm->fCameraPosition[2]);
 }
 
 
@@ -183,23 +242,57 @@ void set_avatar_position(const float x, const float y, const float z) {
 	lm->fAvatarPosition[2] = z;
 }
 
+boost::python::tuple get_avatar_top() {
+	return boost::python::make_tuple(lm->fAvatarFront[0], lm->fAvatarFront[1], lm->fAvatarFront[2]);
+}
+
+boost::python::tuple get_avatar_front() {
+	return boost::python::make_tuple(lm->fAvatarTop[0], lm->fAvatarTop[1], lm->fAvatarTop[2]);
+}
+
+boost::python::tuple get_avatar_position() {
+	return boost::python::make_tuple(lm->fAvatarPosition[0], lm->fAvatarPosition[1], lm->fAvatarPosition[2]);
+}
+
 BOOST_PYTHON_MODULE(mumble_link)
 {
-	python::def("setup", setup);
-	python::def("stop", stop);
-	
-	python::def("set_name", set_name);
-	python::def("set_description", set_description);
-	python::def("set_version", set_version);
-	
-	python::def("set_camera_top", set_camera_position);
-	python::def("set_camera_front", set_camera_front);
-	python::def("set_camera_position", set_camera_position);
+	python::def("open", link_open, "Open the shared memory interface without changing it. Make sure open succeeded before taking any other action.");
 
-	python::def("set_avatar_top", set_avatar_position);
-	python::def("set_avatar_front", set_avatar_front);
-	python::def("set_avatar_position", set_avatar_position);
+	python::def("setup", setup, "Setup as plugin with given data. Start auto ticking. Make sure setup succeeded before taking any other action.");
 
-	python::def("set_context", set_context);
-	python::def("set_identity", set_identity);
+	python::def("start", start, "Start auto ticking every 100ms");
+	python::def("stop", stop, "Stop auto ticking.");
+	
+	python::def("set_name", set_name, "Set plugin name (only takes effect on re-link).");
+	python::def("set_description", set_description, "Set plugin description (only takes effect on re-link).");
+	python::def("set_version", set_version, "Set plugin version. 0 to unlink, 2 to link.");
+	python::def("set_tick", set_tick, "Manually set tick value.");
+	python::def("tick", tick, "Performs a tick by incrementing tick value.");
+
+	python::def("get_name", get_name, "Returns the plugin name.");
+	python::def("get_description", get_description, "Returns the plugin description.");
+	python::def("get_version", get_version, "Returns the plugin version.");
+	python::def("get_tick", get_tick, "Returns the current tick value.");
+	
+	python::def("set_camera_top", set_camera_top, "Sets the top vector for the camera.");
+	python::def("set_camera_front", set_camera_front, "Sets the front vector for the camera.");
+	python::def("set_camera_position", set_camera_position, "Sets the position vector for the camera.");
+
+	python::def("get_camera_top", get_camera_top, "Returns the camera top vector as a x,y,z tuple.");
+	python::def("get_camera_front", get_camera_front, "Returns the camera front vector as a x,y,z tuple.");
+	python::def("get_camera_position", get_camera_position, "Returns the camera position vector as a x,y,z tuple.");
+
+	python::def("set_avatar_top", set_avatar_top, "Sets the top vector for the avatar.");
+	python::def("set_avatar_front", set_avatar_front, "Sets the front vector for the avatar.");
+	python::def("set_avatar_position", set_avatar_position, "Sets the position vector for the avatar.");
+
+	python::def("get_avatar_top", get_avatar_top, "Returns the avatar top vector as a x,y,z tuple.");
+	python::def("get_avatar_front", get_avatar_front, "Returns the avatar front vector as a x,y,z tuple.");
+	python::def("get_avatar_position", get_avatar_position, "Returns the avatar position vector as a x,y,z tuple.");
+
+	python::def("set_context", set_context, "Set the plugin context. Will be prefixed by name inside Mumble. May contain zero values.");
+	python::def("set_identity", set_identity, "Set the plugin identity.");
+
+	python::def("get_context", get_context, "Returns the plugin context. May contain zero values.");
+	python::def("get_identity", get_identity, "Returns the plugin identity.");
 }
